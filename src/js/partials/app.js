597a206f4a87d6c349954dd44b6905f1b30022a2
luft <mpkluft@yandex.ru>
2015-12-07 13:39:06 +0300

    last3

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -2,3 +2,9 @@
 function isEven(val) {
 	return val % 2 === 0;
 }
+
+if(!Modernizr.svg) {
+	$("img[src*='svg']").attr("src", function() {
+		return $(this).attr("src").replace(".svg", ".png");
+	});
+};
